4f3f5fb5cd57fda44094ea7199901f2c6cb842cd
Golubev Yury <gol.y@mail.ru>
2023-07-13 15:54:50 +0300

    Add Variable

---
+++ src/main/java/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = (int) 70.0;
+        System.out.println(myInt);
+
+        char c ='a';
+        int cc = c;
+        System.out.println("c = " + c);
+
+        System.out.println("int in c = " + cc);
+    boolean b = true;
+        System.out.println("b = " + b);
+
+
+    }
+}
